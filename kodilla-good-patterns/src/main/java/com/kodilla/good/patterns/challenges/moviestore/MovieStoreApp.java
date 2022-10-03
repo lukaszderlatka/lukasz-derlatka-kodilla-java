daf3dad5b5d2e3d2b275ff4d9b0a6588f8fc8e56
Łukasz Derlatka <derlatka.lukasz@gmail.com>
2022-10-04 00:22:10 +0200

    Task 13.1 - Functional iteration over a HashMap in MovieStore class

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/moviestore/MovieStoreApp.java
@@ -0,0 +1,10 @@
+package com.kodilla.good.patterns.challenges.moviestore;
+
+public class MovieStoreApp {
+
+	public static void main(String[] args) {
+		MovieStore movieStore = new MovieStore();
+		movieStore.displayMovieTitles();
+	}
+
+}
